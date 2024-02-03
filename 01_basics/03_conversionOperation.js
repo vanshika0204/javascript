0a28b6bff59049b98db2e721c45b9c33a1c36e0c
vanshika0204 <148622391+vanshika0204@users.noreply.github.com>
2024-02-03 07:53:56 +0000

    committed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+let score = "vanshika"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0 
+
+// let isLoggedIn = 1
+// let isLoggedIn =""
+let isLoggedIn = "vanshika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false, "vanshika" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//**** Operations ****
+
+let value=3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = " Vanshika"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(1+"2"+2)
+
+// not preferred :
+// console.log(true)
+// console.log(+true)
+// console.log(true+) => error
+// console.log(+"")
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter)
